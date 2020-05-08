6801969ef9b5b5fad45725fc9ba7abc55d4afff6
zhang1001san <1806988501@qq.com>
2020-05-08 23:17:39 +0800

    登陆

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>首页</h2>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    methods: {
+      logout: function () {
+        window.sessionStorage.clear()
+        this.$router.push('/login')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
